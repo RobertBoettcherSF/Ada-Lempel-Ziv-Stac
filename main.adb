794cd35b0e6037b01a3176734bc7389f81f3b38c
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-24 13:51:55 +0200

    Add main procedure for Lempel-Ziv-Stac library

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+
+procedure Main is
+begin
+   Ada.Text_IO.Put_Line ("Lempel-Ziv-Stac Compression Library successfully built.");
+   Ada.Text_IO.Put_Line ("Please run 'make test' to execute the extensive V&V test suite.");
+end Main;
